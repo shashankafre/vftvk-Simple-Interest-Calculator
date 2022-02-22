981c3848f6062fda543e337e893441e2f33a8612
shashankafre <96933646+shashankafre@users.noreply.github.com>
2022-02-22 13:26:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function updateRate() {
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var year = new Date().getFullYear() + parseInt(years);
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
+function validatecompute() {
+  if (
+    parseInt(document.getElementById("principal").value) <= 0 ||
+    document.getElementById("principal").value === ""
+  ) {
+    alert("Please Enter Non-Zero Positive Values");
+    document.getElementById("principal").focus();
+  } else {
+    compuetinterest();
+  }
+}
+function compuetinterest() {
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var year = new Date().getFullYear() + parseInt(years);
+  var principal = document.getElementById("principal").value;
+  var amount = (principal * years * rate) / 100 + parseInt(principal);
+  document.getElementById("result").innerHTML =
+    "If you deposit <span style='background-color:yellow'>" +
+    principal +
+    "</span>,<br>at an interest rate of <span style='background-color:yellow'>" +
+    rate +
+    "%</span><br>You will receive an amount of <span style='background-color:yellow'>" +
+    amount +
+    "</span>,<br>in the year <span style='background-color:yellow'>" +
+    year +
+    "</span><br>";
+}
